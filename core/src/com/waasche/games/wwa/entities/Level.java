edf764f6dbc2a4117acd43a9c48723b41b5a4ec6
SachokD-TC <Rythm1977>
2017-10-12 18:51:57 +0300

    add Level entity, and change Levels

--- core/src/com/waasche/games/wwa/entities/Level.java
+++ core/src/com/waasche/games/wwa/entities/Level.java
@@ -7,7 +7,29 @@ import java.util.List;
 
 public class Level {
 
-    public Level(String name, List<Rectangle> moves){
+    private String name;
+    private List<Enemy> enemies;
 
+    public Level(String name, List<Enemy> enemies) {
+        this.name = name;
+        this.enemies = enemies;
     }
+
+    public List<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public void setEnemies(List<Enemy> enemies) {
+        this.enemies = enemies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
--- core/src/com/waasche/games/wwa/entities/Levels.java
+++ core/src/com/waasche/games/wwa/entities/Levels.java
@@ -1,12 +1,32 @@
 package com.waasche.games.wwa.entities;
 
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Levels {
 
-    public static String[] levels = {"desert","stones","wood"};
-    public static Rectangle[][] enemies = {{new Rectangle(1160,470,20,200), new Rectangle(200,200,150,150)},
-                                           {new Rectangle(1160,470,20,200), new Rectangle(200,200,150,150)}};
+    public static final String[] levelName = {"desert", "stones", "wood"};
+    private final String[] pics = {"pic/sun.png", "pic/sun.png", "pic/sun.png"};
+    public static Rectangle[][] enemiesRect = {{new Rectangle(1160, 470, 20, 200), new Rectangle(200, 200, 150, 150)},
+            {new Rectangle(1160, 470, 20, 200), new Rectangle(200, 200, 150, 150)}};
+    private List<Level> levels = new ArrayList<>();
 
+    public Levels() {
+        generateLevels();
+    }
+
+    private void generateLevels() {
+        for (int i = 0; i != levelName.length; i++) {
+            List<Enemy> enemies = new ArrayList<>();
+            for (Rectangle rectangle : enemiesRect[i]) {
+                enemies.add(new Enemy(new Sprite(new Texture(pics[i])), 0, rectangle));
+            }
+            levels.add(new Level(levelName[i], enemies));
+        }
+    }
 }
